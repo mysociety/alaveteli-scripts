3688a9de6126dbf5a796b527ac3a44d81acc5d14
Graeme Porteous <graeme@rgbp.co.uk>
2023-01-18 17:11:16 +0000

    fixup! Add script to output Excel attachments received

--- scripts/excel-attachments.rb
+++ scripts/excel-attachments.rb
@@ -7,6 +7,7 @@ unless ENV['PUBLIC_BODY_URL_NAME']
   exit 1
 end
 
+include ActionView::Helpers::NumberHelper
 include Rails.application.routes.url_helpers
 include LinkToHelper
 
@@ -18,24 +19,10 @@ all_attachments = FoiAttachment.joins(
 ).where(public_bodies: { id: pb })
 attachments_of_interest = all_attachments.where('filename ~* ?', '\.xlsx?$')
 
-# From https://stackoverflow.com/a/59927668
-BASE = 1000
-ROUND = 3
-EXP = {0 => '', 1 => 'k', 2 => 'M', 3 => 'G', 4 => 'T'}
-def file_size_humanize(file_size)
-  EXP.reverse_each do |exp, char|
-    if (num = file_size.to_f / BASE**exp) >= 1
-      rounded = num.round ROUND
-      n = (rounded - rounded.to_i > 0) ? rounded : rounded.to_i
-      return "#{n} #{char}B"
-    end
-  end
-end
-
 attachments_of_interest.each do |attachment|
   embargoed = attachment.incoming_message.info_request.embargo.present?
   print (embargoed ? 'EMBARGOED:' : 'PUBLIC:').ljust(11)
   size = attachment.file.blob.byte_size
-  print file_size_humanize(size).rjust(10) + ' '
+  print number_to_human_size(size).rjust(7) + ' '
   puts foi_attachment_url(attachment)
 end
