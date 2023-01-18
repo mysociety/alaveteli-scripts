307301f017e71f7b5fded046cf2d8bc36b4b77f4
Graeme Porteous <graeme@rgbp.co.uk>
2023-01-18 09:24:38 +0000

    Add script to output Excel attachments received
    Requires the PUBLIC_BODY_URL_NAME environment variable to scope the
    output to a single authority.

---
+++ scripts/excel-attachments.rb
@@ -0,0 +1,25 @@
+# Run using rails runner, EG:
+# PUBLIC_BODY_URL_NAME=url_name bin/rails runner excel-attachments.rb
+#
+
+unless ENV['PUBLIC_BODY_URL_NAME']
+  puts 'Requires PUBLIC_BODY_URL_NAME envrionment variable'
+  exit 1
+end
+
+include Rails.application.routes.url_helpers
+include LinkToHelper
+
+default_url_options[:host] = AlaveteliConfiguration.domain
+
+pb = PublicBody.find_by!(url_name: ENV['PUBLIC_BODY_URL_NAME'])
+all_attachments = FoiAttachment.joins(
+  incoming_message: { info_request: [:public_body] }
+).where(public_bodies: { id: pb })
+attachments_of_interest = all_attachments.where('filename ~* ?', '\.xlsx?$')
+
+attachments_of_interest.each do |attachment|
+  embargoed = attachment.incoming_message.info_request.embargo.present?
+  print embargoed ? 'EMBARGOED: ' : 'PUBLIC   : '
+  puts foi_attachment_url(attachment)
+end
